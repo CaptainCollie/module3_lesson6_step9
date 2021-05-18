419761e6be4facb9780e882df176e0625dc97797
CaptainCollie <shadaraevd@gmail.com>
2021-05-18 11:51:12 +0800

    add step solution

--- solution/test_items.py
+++ solution/test_items.py
@@ -6,4 +6,4 @@ def test_button_add_to_basket_check(browser):
     browser.get(link)
     time.sleep(30)
     button_text = browser.find_element_by_css_selector(".btn-add-to-basket").text
-    assert button_text == "Añadir al carrito", f'expected "Añadir al carrito", got {button_text}'
+    assert button is not None, "button is not found"
