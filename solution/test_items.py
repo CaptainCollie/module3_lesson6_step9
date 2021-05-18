c9453b80fea76fb6efd101ef35e97df91e6554f9
CaptainCollie <shadaraevd@gmail.com>
2021-05-18 11:29:26 +0800

    add step solution

--- solution/test_items.py
+++ solution/test_items.py
@@ -2,7 +2,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_browser_add_button_check(browser):
+def test_button_add_to_baskcet_check(browser):
     browser.get(link)
     time.sleep(30)
     button_text = browser.find_element_by_css_selector(".btn-add-to-basket").text
