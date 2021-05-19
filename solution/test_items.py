9ba3a47d50801fdfbefc204e3471e7949c888016
CaptainCollie <30548957+CaptainCollie@users.noreply.github.com>
2021-05-19 15:02:16 +0800

    Update test_items.py

--- solution/test_items.py
+++ solution/test_items.py
@@ -5,5 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button_add_to_basket_check(browser):
     browser.get(link)
     time.sleep(30)
-    button_text = browser.find_element_by_css_selector(".btn-add-to-basket").text
+    button = browser.find_element_by_css_selector(".btn-add-to-basket").text
     assert button is not None, "button is not found"
